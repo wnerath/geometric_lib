d5d6a5460f3da5c9e848c1c86f6e5d9de37ac916
Sergey Kukharenko <sergeykukharenko@MacBook-Air-Sergey-2.local>
2024-11-25 19:26:32 +0300

    feat: add radius check

--- circle.py
+++ circle.py
@@ -2,11 +2,15 @@ import math
 # Импортирует библиотеку math чтобы использовать функцию math.pi для числа пи
 
 def area(r):
+    if r < 0:
+        raise ValueError("Radius cannot be negative") # Проверка радиуса
     return math.pi * r * r
    
     # Возвращает площадь окружности по полученному радиусу
     
 def perimeter(r):
+    if r < 0:
+        raise ValueError("Radius cannot be negative")  # Проверка радиуса
     return 2 * math.pi * r
 
     # Возвращает периметр окружности по полученному радиусу
