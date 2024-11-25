2e1fd8dda5ef9b430c784d1df70ed70ddb898ffc
Sergey Kukharenko <sergeykukharenko@MacBook-Air-Sergey-2.local>
2024-11-25 19:30:26 +0300

    feat: add side check

--- square.py
+++ square.py
@@ -1,9 +1,12 @@
-
 def area(a):
+    if a < 0:
+        raise ValueError("Side length cannot be negative")  # Проверка стороны
     return a * a
     # Возвращает площадь квадрата по полученной стороне, при a = 3 вернет 9
 
 
 def perimeter(a):
+    if a < 0:
+        raise ValueError("Side length cannot be negative")  # Проверка стороны
     return 4 * a
     # Возвращает периметр квардрата по полученной стороне, при a = 3 вернет 12
