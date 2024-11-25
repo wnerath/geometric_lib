b7e1d1a817429cf240612e64434220b6df9e8c50
Sergey Kukharenko <sergeykukharenko@MacBook-Air-Sergey-2.local>
2024-11-25 19:33:25 +0300

    feat: add side check

--- rectangle.py
+++ rectangle.py
@@ -1,6 +1,10 @@
-def area(a, b): 
-    return a * b 
+def area(a, b):
+    if a < 0 or b < 0:
+        raise ValueError("Side lengths cannot be negative")  # Проверка сторон
+    return a * b
     # Возвращает площадь прямоугольника по полученным двум сторонам 
-def perimeter(a, b): 
+def perimeter(a, b):
+    if a < 0 or b < 0:
+        raise ValueError("Side lengths cannot be negative")  # Проверка сторон
     return a*2 + b*2
     # Возвращает периметр прямоугольника по полученным двум сторонам 
